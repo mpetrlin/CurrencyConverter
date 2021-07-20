3a727762b283aed79dfc11622c12c12e66f76d2c
Matej Petrlin <matej.petrlin@outlook.com>
2021-07-20 11:34:33 +0200

    trial_3

--- API/Startup.cs
+++ API/Startup.cs
@@ -60,7 +60,7 @@ namespace API
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
 
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};TrustServerCertificate=True";
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";
                 }
 
                     // Whether the connection string came from the local development configuration file
